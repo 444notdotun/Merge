ac456f9600ec56cbfa7561c4cc2323a83030ef94
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-07-02 12:45:28 -0700

    Add findByIdAndStudentId to SessionRepository for ownership-safe session lookup

--- backend/src/main/java/com/merge/backend/engagement/repository/SessionRepository.java
+++ backend/src/main/java/com/merge/backend/engagement/repository/SessionRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SessionRepository extends JpaRepository<Session, String> {}
+public interface SessionRepository extends JpaRepository<Session, String> {
+
+    java.util.Optional<Session> findByIdAndStudentId(String id, Long studentId);
+}
